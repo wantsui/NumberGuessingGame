82d94a2d1c2ededaff19c11e51fc3b5135a984b4
Wan <wanytsui@gmail.com>
2017-08-12 23:17:02 -0400

    Add readme and add a box component.

---
+++ README.md
@@ -0,0 +1,14 @@
+### Number Guessing Game in VR
+
+
+This is an in progress game using React VR for guessing a number! I am still learning how React works, so there will be updates occasionally!
+
+Please check back soon for more details.
+
+**Resources**
+
+1) React VR (GitHub)
+ - https://github.com/facebook/react-vr
+
+2) React VR (Documentation)
+  - https://facebook.github.io/react-vr/
---
+++ components/FloatingBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  VrButton,
+  Box,
+  View,
+} from 'react-vr';
+export default class FloatingBox extends React.Component {
+  render() {
+    let layoutX = this.props.layoutX
+    let layoutY = this.props.layoutY
+    let translateZ = this.props.translateZ
+    let boxColor = this.props.boxColor
+    let comment = this.props.comment
+    return(
+        <VrButton style={{layoutOrigin: [layoutX, layoutY],
+                        transform: [{translate: [1, 2, translateZ]}]}}
+                  onClick={() => console.log(comment)}>
+          <Box
+            dimHeight={.5}
+            dimWidth={.5}
+            dimDepth={.5}
+            style={{
+              color: boxColor,
+            }}
+          />
+        </VrButton>
+    );
+  }
+}
